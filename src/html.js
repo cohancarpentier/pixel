6fa30eed58f6accf81bf4a2745616aea4c75d42d
Cohan Carpentier <cohan.carpentier@logmein.com>
2019-02-21 17:24:33 -0500

    removed slick from html

--- src/html.js
+++ src/html.js
@@ -16,17 +16,6 @@ export default function HTML(props) {
           content="A page's description, usually one or two sentences."
         />
         {props.headComponents}
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
