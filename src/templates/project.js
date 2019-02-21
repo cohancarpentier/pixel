855174585a3e580a6d7dd80753d1babb2f0bafe2
Cohan Carpentier <cohan.carpentier@logmein.com>
2019-02-21 08:59:53 -0500

    more project images

--- src/templates/project.js
+++ src/templates/project.js
@@ -8,14 +8,21 @@ import squares from './../images/squares.svg'
 import projectStyles from './project.module.scss'
 
 class ProjectTemplate extends React.Component {
+  state = {
+    descriptionOpened: false,
+  }
+
   render() {
-    const post = get(this.props, 'data.contentfulProject')
+    const project = get(this.props, 'data.contentfulProject')
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
     const siteMetadata = get(this, 'props.data.site.siteMetadata')
+    const { descriptionOpened } = this.state
+
+    console.log(project)
 
     return (
       <Layout location={this.props.location} siteMetadata={siteMetadata}>
-        <Helmet title={`${post.title} | ${siteTitle}`} />
+        <Helmet title={`${project.title} | ${siteTitle}`} />
 
         <div className="has-background-black" style={{ paddingTop: '8rem' }}>
           <div className="container is-fluid">
@@ -24,17 +31,69 @@ class ProjectTemplate extends React.Component {
             />
             <div
               className="level has-text-white has-text-weight-semibold"
-              style={{ paddingTop: '2rem' }}
+              style={{ paddingTop: '2rem', paddingBottom: '2rem' }}
             >
               <div className="level-left">
                 <h1 className="has-text-weight-semibold">Portfolio</h1>
               </div>
               <div className="level-right">
-                <h2>description</h2>
+                <a
+                  onClick={() =>
+                    this.setState({
+                      descriptionOpened: !descriptionOpened,
+                    })
+                  }
+                  className="has-text-white"
+                  style={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <span
+                    className="title is-3 has-text-white"
+                    style={{ fontWeight: 100, marginRight: '1rem' }}
+                  >
+                    +
+                  </span>
+                  Description du projet
+                </a>
               </div>
             </div>
           </div>
         </div>
+
+        {descriptionOpened ? (
+          <div className="has-background-black">
+            <div className="container is-fluid">
+              <hr
+                style={{ backgroundColor: 'rgba(255,255,255,0.1', margin: 0 }}
+              />
+              <div
+                className="columns has-text-white has-text-weight-semibold"
+                style={{ paddingTop: '4rem', paddingBottom: '6rem' }}
+              >
+                <div className="column is-7">
+                  <h1 className="title is-5 has-text-white has-text-weight-bold">
+                    {project.title}
+                  </h1>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: project.body.childMarkdownRemark.html,
+                    }}
+                  />
+                </div>
+                <div className="column is-3 is-offset-2">
+                  <h2 className="has-text-weight-bold">Client</h2>
+                  <p>{project.client}</p>
+                  <hr style={{ backgroundColor: 'rgba(255,255,255,0.1)' }} />
+                  <h2 className="has-text-weight-bold">Services</h2>
+                  <p>{project.client}</p>
+                  <hr style={{ backgroundColor: 'rgba(255,255,255,0.1)' }} />
+                  <h2 className="has-text-weight-bold">Autres informations</h2>
+                  <p>{project.client}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : null}
+
         <div>
           <div
             className="container has-background-white is-fluid"
@@ -43,23 +102,18 @@ class ProjectTemplate extends React.Component {
               marginBottom: '4rem',
             }}
           >
-            <Img alt={post.title} fluid={post.heroImage.fluid} />
+            <Img alt={project.title} fluid={project.heroImage.fluid} />
 
-            <h1 className="section-headline">{post.title}</h1>
-            <p
-              style={{
-                display: 'block',
-              }}
-            >
-              {post.publishDate}
-            </p>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: post.body.childMarkdownRemark.html,
-              }}
-            />
+            <div className="columns" style={{ marginTop: '1rem' }}>
+              <div className="column is-half">
+                <Img alt={project.title} fluid={project.heroImage.fluid} />
+              </div>
+              <div className="column is-half">
+                <Img alt={project.title} fluid={project.heroImage.fluid} />
+              </div>
+            </div>
 
-            <div className="level">
+            <div className="level" style={{ marginTop: '4rem' }}>
               <div className="level-left">
                 <Link
                   to={'/projects'}
