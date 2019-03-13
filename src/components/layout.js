5847db4ece7e296d0b842cf96543c37a6e2b4478
Cohan Carpentier <cohan.carpentier@logmein.com>
2019-03-13 10:17:00 -0400

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from 'react'
 import './../styles/styles.scss'
-if (window.location.pathname.replace(/^\/([^\/]*).*$/, '$1') === 'en') {
-  require('./../styles/_glitchy-en.scss')
-} else {
-  require('./../styles/_glitchy-fr.scss')
+if (typeof window !== 'undefined') {
+  if (window.location.pathname.replace(/^\/([^\/]*).*$/, '$1') === 'en') {
+    require('./../styles/_glitchy-en.scss')
+  } else {
+    require('./../styles/_glitchy-fr.scss')
+  }
 }
 import { Navigation } from './navigation'
 import { Footer } from './footer'
